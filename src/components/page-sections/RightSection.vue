c267f3892988d3cf7c5c4d824af5bde129c512ed
Said El bourabi <said.elbourabi@bosa.fgov.be>
2022-10-31 23:46:43 +0100

    make chart dynamic

--- src/components/page-sections/RightSection.vue
+++ src/components/page-sections/RightSection.vue
@@ -3,30 +3,47 @@
         <v-card-title class="mt-5 ml-3">
             {{ question }}
         </v-card-title>
-        <v-card-actions v-for="n in options" :key="n">
+        <v-card-actions v-for="n in voteStore.getAnswers" :key="n">
             <v-row>
                 <v-col cols="12">
-                    <v-progress-linear :model-value="n.value" :color="n.color" height="25">
-                        {{ n.name }} - {{ n.value }} vote(s)
+                    <v-progress-linear :model-value="voteStore.getVotes.length" color="blue" height="25">
+                        {{ n.value }} - {{ voteStore.getVotes.length }} vote(s)
                     </v-progress-linear>
                 </v-col>
             </v-row>
         </v-card-actions>
-        <v-card-title class="mt-5 ml-1">
+        <v-card-title class="cardTitle">
             Total votes: {{ totalVotes }}
         </v-card-title>
     </v-card>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { useVoteStore } from '@/store';
+import { computed } from 'vue';
 
-const question = 'What is your question ?'
-const totalVotes = ref(20)
-const options = ref([
-    { name: 'option 1', value: 10, color: 'primary' },
-    { name: 'option 2', value: 50, color: 'secondary' },
-    { name: 'option 3', value: 70, color: 'warning' },
-])
+const voteStore = useVoteStore()
+
+const question = computed(() => {
+    return voteStore.getQuestion
+})
+
+const totalVotes = computed(() => {
+    return voteStore.getVotes.length
+})
+
+/*const options = computed(() => {
+    return [
+        ...voteStore.getAnswers.map((vote) => {
+            return {
+                ...vote,
+                name: vote.value,
+                value: 1,
+                color: 'green' 
+            } 
+        })
+    ]
+}) */
+
 </script>
 <style scoped>
 .card {
@@ -34,13 +51,9 @@ const options = ref([
     min-height: 100%;
 }
 
-.cardActions {
+.cardTitle {
     bottom: 0px;
     min-width: 100%;
     position: absolute;
-}
-
-.voteBtn {
-    min-width: 100%;
 }
 </style>
