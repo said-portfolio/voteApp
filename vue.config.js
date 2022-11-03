52e272a649a5945cb77e953d19acba6479c5db51
Said El bourabi <said.elbourabi@bosa.fgov.be>
2022-11-03 14:34:01 +0100

    adding public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/deploying-sir-vote-a-lot-project',
   transpileDependencies: true,
   pluginOptions: {
     vuetify: {
